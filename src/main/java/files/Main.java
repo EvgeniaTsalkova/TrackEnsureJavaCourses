1f8b90b3b9b2f9b1ee68041440b2165df043fb7f
EvgeniaTsalkova <82383774+EvgeniaTsalkova@users.noreply.github.com>
2021-10-10 19:54:52 +0300

    Commit 1

--- src/main/java/files/Main.java
+++ src/main/java/files/Main.java
@@ -1,41 +1,48 @@
 package files;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Enter the path to the start directory:");
+          try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            String path = bufferedReader.readLine();
+            Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                        System.out.println("Visiting file: " + file);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-      try {
-            Files.walkFileTree(Paths.get("F://Фильмы"), new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                    System.out.println("Visiting file: " + file);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
 
-        /* Второй вариант
-          try {
-             Files.walk(startingDir)
+        /* // Второй вариант
+          try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))){
+            String path = bufferedReader.readLine();
+            Files.walk(path)
                     .filter(Files::isRegularFile)
                     .forEach(System.out::println);
-         }
-         catch(IOException e){
+         } catch (IOException e) {
              e.printStackTrace();
          } */
 
-        /* Третий вариант
-        File dir = new File("F://Фильмы");
-        readFiles(dir);
-         */
+        /* // Третий вариант
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            File dir = new File(bufferedReader.readLine());
+            readFiles(dir);
+        } catch (IOException e){
+            e.printStackTrace();
+        } */
+
 
     }
     static void readFiles(File start){
@@ -49,6 +56,5 @@ public class Main {
                     }
                 }
             }
-
     }
 }
